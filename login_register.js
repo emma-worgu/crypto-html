b2fcaf546a292047dc142ece88669c0b89b7c617
Emmanuel <emmanuelworgu@gmail.com>
2021-12-29 21:52:57 +0100

    site shutdown

--- login_register.js
+++ login_register.js
@@ -40,40 +40,44 @@ async function login () {
 
 async function register () {
   try {
-    document.getElementById('btn-text').innerHTML = 'Please Wait...';
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const ip = localStorage.getItem('ip');
-    // const url = 'https://crypto-backend1.herokuapp.com/api/user/login/';
-
-    const url = 'https://crypto-backend1.herokuapp.com/api/user/register/';
-
-    const body = {
-      name,
-      email,
-      password,
-      ip,
-    }
-
-    const req = await fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(body),
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      }
-    });
-
-    const res = await req.json();
-    console.log(res);
-
-    if (req.status !==200) {
-      document.getElementById('btn-text').innerHTML = 'Sign In';
-      alert(res.message);
+    if (document.location.origin === 'https://meta3trader.com') {
+      alert('Can not register at this time. Please Try again later');
     } else {
-      localStorage.setItem('token', res.token);
-      document.location.href = '/dash.html';
+      document.getElementById('btn-text').innerHTML = 'Please Wait...';
+      const name = document.getElementById('name').value;
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+      const ip = localStorage.getItem('ip');
+      // const url = 'https://crypto-backend1.herokuapp.com/api/user/login/';
+
+      const url = 'https://crypto-backend1.herokuapp.com/api/user/register/';
+
+      const body = {
+        name,
+        email,
+        password,
+        ip,
+      }
+
+      const req = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        }
+      });
+
+      const res = await req.json();
+      console.log(res);
+
+      if (req.status !==200) {
+        document.getElementById('btn-text').innerHTML = 'Sign In';
+        alert(res.message);
+      } else {
+        localStorage.setItem('token', res.token);
+        document.location.href = '/dash.html';
+      }
     }
   } catch (error) {
     document.getElementById('btn-text').innerHTML = 'Sign In';
